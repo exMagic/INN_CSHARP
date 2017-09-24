c5bbb5a32afffb6f3c8baab2d0ed798f7ac5122a
Maciek-PC <Maciek@DESKTOP-PC>
2017-09-24 20:29:28 +0200

    sql query

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+select*from flowers
+
+insert into flowers (variety, colour, plu, farm_id, barcode, mix, sticker_text, lenght, bucket_size, sleeve_type, fob, price_pr_bunch, fairtrade, bunch_pr_box, bunch_pr_bucket, stems_pr_bunch, pake_rete)
+values ('mirabel', 'red', '123123', '1', '121212333333', '0', 'mirabel' , '40', '15', 'sleeve 1', '2.1', '1.23', '1', '30','4', '15', '320') ;  
+
+
+select*from farms
+insert into farms (farm_id,farm_name, farm_code)
+values ('1','Primarosa', '121212') ;  
